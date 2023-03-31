b038641e1c049a88b1cd7503e5daa29c524d86eb
Oliver Ramstedt <ora@capasystems.com>
2023-03-31 14:57:45 +0200

    refactor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,16 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return(
     <ClerkProvider {...pageProps} >
+      <Head>
+        <title>Birber</title>
+        <meta name="description" content="🦜" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster 
         position="bottom-center" 
         toastOptions={{
