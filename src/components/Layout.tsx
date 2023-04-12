5c2f6b3426cd4c180cb94b58ebfb4525b42b53ae
Oliver Ramstedt <ora@capasystems.com>
2023-04-12 15:24:10 +0200

    border color change

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import type { PropsWithChildren } from "react";
 export const PageLayout = (props: PropsWithChildren) => {
     return (
         <main className="flex justify-center h-screen">
-            <div className="w-full md:max-w-4xl border-x h-full border-slate-400 overflow-y-scroll">
+            <div className="w-full md:max-w-4xl border-x h-full border-slate-600 overflow-y-scroll">
                 {props.children}
             </div>
         </main>
