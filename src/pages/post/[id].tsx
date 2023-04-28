9e2d1bc26599f4d90604613b0c63153c839bd047
Oliver Ramstedt <ora@capasystems.com>
2023-04-28 13:30:27 +0200

    Single post page

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,20 +1,49 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import type { GetStaticProps } from "next";
+import { api } from "~/utils/api";
+import { PageLayout } from "~/components/Layout";
+import { generateSSgHelper } from "~/server/helpers/ssgHelper";
+import { PostView } from "~/components/PostView";
 
-const SinglePostPage: NextPage = () => {
+const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
+  const { data } = api.posts.getById.useQuery({
+    id,
+  });
+
+  if(!data) return <div>404</div>;
 
   return (
     <>
       <Head>
-        <title>Post</title>
+        <title>{`${data.post.content} - ${data.author.username}`}</title>
       </Head>
-      <main className="flex justify-center h-screen">
-        <div>
-          Post View
-        </div>
-      </main>
+      <PageLayout>
+        <PostView { ...data } />
+      </PageLayout>
     </>
   );
 };
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSgHelper();
+
+  const id = context.params?.id;
+
+  if(typeof id !== "string") throw new Error("No id provided");
+
+  await ssg.posts.getById.prefetch({ id });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths:[], fallback: "blocking" };
+}; 
 
 export default SinglePostPage;
